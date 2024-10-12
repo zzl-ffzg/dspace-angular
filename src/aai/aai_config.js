61eaf89594e6ce49c37662db49a4f2c7f2584a26
Vanja Štefanec <vanja.stefanec@broj42.com>
2024-10-12 15:02:07 +0200

    Translations and aai config

--- src/aai/aai_config.js
+++ src/aai/aai_config.js
@@ -25,10 +25,10 @@ jQuery(document).ready(
                             "themes/UFAL/lib/html/disco-juice.html?";
             // e.g., instance.metadataFeed = "http://localhost:8080/server/api/discojuice/feeds?callback=dj_md_1";
             instance.metadataFeed = instance.target + "discojuice/feeds";
-            instance.serviceName = "LINDAT/CLARIAH-CZ Repository";
+            instance.serviceName = "HR-CLARIN Repository";
             instance.localauth =
                 '<form method="post" action="' + instance.target + 'password-login"> ' +
-                    '<p>Sign in using your local account obtained from the LINDAT/CLARIAH-CZ administrator.</p>' +
+                    '<p>Sign in using your local account obtained from the HR-CLARIN administrator.</p>' +
                     '<p style="margin: 5px; color: #888" ><input type="text" name="login_email" style="font-size: 160%; width: 100%" id="login" /> <label for="login">E-Mail Address</label></p>' +
                     '<p style="margin: 5px; color: #888" ><input type="password" name="login_password" style="font-size: 160%; width: 100%" id="pass" /> <label for="pass">Password</label></p>' +
                     '<p style="margin: 5px; color: #607890; text-decoration: underline;"><a href="' + instance.target + 'forgot">Forgot your password?</a></p>' +
@@ -38,7 +38,7 @@ jQuery(document).ready(
             return instance;
         })();
         if (!("aai" in window)) {
-            throw "Failed to find UFAL AAI object. See https://redmine.ms.mff.cuni.cz/projects/lindat-aai for more details!";
+            throw "Failed to find AAI@EduHr object. See https://www.srce.unizg.hr/en/aai for more details!";
         }
         window.aai.setup(opts);
     }
